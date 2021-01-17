91755e5f44d1f93d345c7d6db53a1dfff5ac4575
Carlos Roberto dos Santos Silva <tenroberto2011@gmail.com>
2021-01-17 08:32:03 -0300

    Ajustes no Main

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,6 +1,6 @@
 module "users" {
   
-  sources = "../infra/users"
+  source = "../../infra/users"
   environment = "${var.environment}"
   write_capacity = "${var.write_capacity}"
   read_capacity = "${var.read_capacity}"
