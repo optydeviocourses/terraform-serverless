7ba3a52fab2abe0a61d3315a7a2ae88efb51f0da
optydeviocourses <optydev.io.courses@gmail.com>
2021-01-18 08:39:37 -0300

    Role e Policy of register

---
+++ terraform/infra/users/iam-policy-register.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_policy" "register_policy" {
+  name        = "${var.environment}-register-policy"
+  
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:PutItem"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_dynamodb_table.users.arn}"
+    }
+  ]
+}
+EOF
+}
