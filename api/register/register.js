97f8ceee53ad2f129e0e89643f7261e8613448ef
Carlos Roberto dos Santos Silva <tenroberto2011@gmail.com>
2021-01-20 11:16:39 -0300

    Ajuste sem sucesso ...

--- api/register/register.js
+++ api/register/register.js
@@ -1,16 +1,32 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+
+AWS.config.update(
+  {
+   region: process.env.AWS_REGION,
+  }
+)
+
+const documentCliente = new AWS.DocumentClient()
+const bcrypt = require('bcryptjs')
+const uuid = require('uuid/v4')
+
 module.exports.register = async event => {
+  const body = JSON.parse(event.body)
+  await documentCliente.put({
+    TableName: process.env.DYNAMODB_USERS,
+    Item: {
+      id: uuid(),
+      name: body.name,
+      email: body.email,
+      password: bcrypt.hashSync(body.password, 10)
+    }
+  }).promise()
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: process.env.DYNAMODB_USERS,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-  
+    statusCode: 201,
+    body: JSON.stringify({ message: 'Usuário inserido com sucesso' })
+  }
+
 };
