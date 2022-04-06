540b66a98b862de4a3f4550bf73a3cf605f8500c
optydeviocourses <optydev.io.courses@gmail.com>
2022-04-06 18:54:10 -0300

    Aula 29

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,7 +1,9 @@
 module "users" {
-  source= "../../infra/users"
-  environment = "${var.environment}"
-  write_capacity = "${var.write_capacity}"
-  read_capacity = "${var.read_capacity}"
-  region = "${var.region}"
+  source         = "../../infra/users"
+  environment    = var.environment
+  write_capacity = var.write_capacity
+  read_capacity  = var.read_capacity
+  region         = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
--- terraform/infra/users/variables.auto.tfvars
+++ terraform/infra/users/variables.auto.tfvars
@@ -1,4 +1,6 @@
 environment    = "dev"
+access_key     = "AKIA5LZOAHQXD2PWO7UC"
+secret_key     = "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"
 region         = "us-east-1"
 write_capacity = 1
 read_capacity  = 1
